892450542cd209cbab95f75a21f5d329b6fee4f1
kjinabc <kjinabc@gmail.com>
2022-11-19 02:28:21 +0900

    actorserializers

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie,Genre
 
 class MovieListSerializer(serializers.ModelSerializer):
     # liked_user_set =
@@ -7,10 +7,18 @@ class MovieListSerializer(serializers.ModelSerializer):
         model = Movie
         fields = ('id','genre','title','poster_path','popularity','release_date',
                     'vote_average','vote_count')
+        
+        
+        
 
 class MovieDetailSerializer(serializers.ModelSerializer):
+    class GenreNameSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Genre
+            fields = '__all__'
+    genre = GenreNameSerializer(many=True)
+    
     # liked_user_set =
-
     class Meta:
         model = Movie
         fields = '__all__'
